d7cd2d07a4dd943659d45302c78650e2a47a74e4
Kelly Norton <knorton@dhcp-172-29-213-217.atl.corp.google.com>
2010-01-21 15:14:54 -0500

    More minor changes.

--- sdm_bot.py
+++ sdm_bot.py
@@ -12,6 +12,8 @@ class MockClient:
     return 7420
 
   def report_result(self, agent, branch, revision, result):
+    # This is also used to communicate errors in building and running
+    # tests.
     return True
 
 class Agent:
@@ -23,16 +25,16 @@ class Agent:
     self.branch = branch
     self.name = name
 
-  def working_dir_for_branch(self):
+  def _path_for_branch(self):
     return os.path.join(self.working_dir, self.branch)
 
-  def working_dir_for_tools(self):
+  def _path_for_tools(self):
     return os.path.join(self.working_dir, 'tools')
 
-  def url_for_branch(self):
+  def _url_for_branch(self):
     return Agent.BASE_URL + self.branch
 
-  def url_for_tools(self):
+  def _url_for_tools(self):
     return Agent.BASE_URL + 'tools'
 
   def _updateWorkingCopy(self, svn, url, path, revision):
@@ -41,6 +43,13 @@ class Agent:
       svn.update(path, revision = revision)
     else:
       svn.checkout(url, path, revision = revision)
+
+  def _execute(self, command, cwd = None):
+    if not cwd:
+      cwd = self._path_for_branch()
+    proc = subprocess.Popen(command, cwd = cwd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    return (proc.returncode, stdout, stderr)
 
   def start(self):
     client = self.client
@@ -53,24 +62,22 @@ class Agent:
     # TODO(knorton):
     # (1) Checkout/Update GWT branch to revision.
     print "Updating %s" % self.branch
-    self._updateWorkingCopy(svn, self.url_for_branch(), self.working_dir_for_branch(), revision)
+    self._updateWorkingCopy(svn, self._url_for_branch(), self._path_for_branch(), revision)
     # (2) Checkout/Update GWT tools to HEAD.
     print "Updating tools"
-    self._updateWorkingCopy(svn, self.url_for_tools(), self.working_dir_for_tools(), revision)
+    self._updateWorkingCopy(svn, self._url_for_tools(), self._path_for_tools(), revision)
     # (3) Build GWT
-    # TODO(knorton): Capture stdout/stderr
     # TODO(knorton): Check ant and don't assume it's on the path.
-    p = subprocess.Popen("ant", cwd = self.working_dir_for_branch())
-    p.communicate() 
-    assert p.returncode
-      
+    status, stdout, stderr = self._execute("ant")
+    if status != 0:
+      print "Build Failed"
     # (4) Run Tests.
 
   def run(self):
     svn = pysvn.Client()
     last_revision = self.last_revision
     if last_revision:
-      log = svn.log(self.url_for_branch(), revision_end = pysvn.Revision(pysvn.opt_revision_kind.number, last_revision + 1))
+      log = svn.log(self._url_for_branch(), revision_end = pysvn.Revision(pysvn.opt_revision_kind.number, last_revision + 1))
       log.reverse()
       print "Processing %d revisions." % len(log)
       for change in log:
